789931770761448ef89497a7de2585d4d338014b
vazgensoghoyan <sogvazulchik@gmail.com>
2025-11-14 15:02:05 +0300

    primary processing in sqrt2 circuit in verilog

--- sqrt2.sv
+++ sqrt2.sv
@@ -8,20 +8,49 @@ module sqrt2(
     input   wire ENABLE
 ); 
     
+    // считываем входные данные на первом такте
     reg [15:0] given_input;
     reg loaded = 0;
 
     always @(posedge CLK) begin
-        if (ENABLE && !loaded) begin
+        if (!ENABLE) begin
+            loaded <= 0;
+        end if (ENABLE && !loaded) begin
             given_input <= IO_DATA;
             loaded <= 1;
         end
     end
 
+    // счетчик
     wire[7:0] counter_value;
     counter _counter(counter_value, CLK, ENABLE);
 
-    assign IO_DATA = (loaded && counter_value >= 2) ? given_input : 16'hZZZZ;
+    // первичная обработка
+    wire[9:0] mant;
+    wire[4:0] exp;
+    wire sign;
+    wire is_input_zero, is_input_denorm, is_input_nan, is_input_inf;
+
+    input_parser _ip(
+        mant, exp, sign, is_input_zero, is_input_denorm, 
+        is_input_nan, is_input_inf, given_input
+    );
+
+    reg[15:0] special_out;
+    wire is_nan, is_pinf, is_ninf, bypass_core;
+
+    special_case_handler _sch(
+        special_out, is_nan, is_pinf, is_ninf, bypass_core, 
+        given_input, sign, is_input_zero, is_input_nan, is_input_inf
+    );
+
+    // со второго такта выводим результат
+    assign IO_DATA = (loaded && counter_value >= 2) ? special_out : 16'hZZZZ;
+    assign IS_NAN = is_nan && counter_value >= 2;
+    assign IS_PINF = is_pinf && counter_value >= 2;
+    assign IS_NINF = is_ninf && counter_value >= 2;
+
+    assign RESULT = bypass_core;
 
 endmodule
 
@@ -48,16 +77,16 @@ module input_parser(
 endmodule
 
 module special_case_handler(
-    output reg  [15:0] SPECIAL_OUT,
-    output wire        IS_NAN,
-    output wire        IS_PINF,
-    output wire        IS_NINF,
-    output wire        BYPASS_CORE,
-    input  wire [15:0] VALUE,
-    input  wire        SIGN,
-    input  wire        IS_ZERO_INP,
-    input  wire        IS_NAN_INP,
-    input  wire        IS_INF_INP
+    output reg[15:0] SPECIAL_OUT,
+    output wire IS_NAN,
+    output wire IS_PINF,
+    output wire IS_NINF,
+    output wire BYPASS_CORE,
+    input  wire[15:0] VALUE,
+    input  wire SIGN,
+    input  wire IS_ZERO_INP,
+    input  wire IS_NAN_INP,
+    input  wire IS_INF_INP
 );
 
     assign IS_NAN  = IS_NAN_INP || (IS_INF_INP && SIGN) || (~IS_ZERO_INP && SIGN);
@@ -81,11 +110,11 @@ module special_case_handler(
 endmodule
 
 module counter(output reg[7:0] OUT, input CLK, ENABLE);
-  always @ (posedge CLK) begin
+  always @(posedge CLK) begin
     if (ENABLE) begin
-        OUT = OUT + 1; 
+        OUT <= OUT + 1; 
     end else begin
-      OUT = 0;
+      OUT <= 0;
     end
   end
 endmodule
